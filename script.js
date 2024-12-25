f2a3cfac95c3c7b22158cf331abf081ece447968
MohammadAsad-Weber <mohdasad69690@gmail.com>
2024-12-25 12:48:10 +0530

    Updated JS File

--- script.js
+++ script.js
@@ -1,58 +1,61 @@
-import { apiKey } from './ApiKey.js';
+import { apiKey } from "./ApiKey.js"; // API Key
 
+// Elements
 const Input = document.getElementById(`Input`);
 const Pages = document.getElementById(`Pages`);
 const Search = document.getElementById(`Search`);
 const Increase = document.getElementById(`Increase`);
 
+// Page Number
 let Page = 1;
 
+// Total Pages
+let totalPages = 0;
+
+// Function for Retrieving Images
 async function Retrieve() {
-    const response = await fetch(`https://api.unsplash.com/search/photos?page=${Page}&query=${Input.value}&per_page=12&content_filter=high&client_id=${apiKey}`);
-    const data = await response.json();
-    const Result = data.results;
+    try {
+        const response = await fetch(`https://api.unsplash.com/search/photos?page=${Page}&query=${Input.value}&per_page=12&content_filter=high&client_id=${apiKey}`);
 
-    for (let index of Result) {
-        const img = document.createElement(`img`);
-        img.src = index.urls.small;
+        const data = await response.json();
+        const Result = data.results;
+        Page = data.total_pages;
 
-        const a = document.createElement(`a`);
-        a.href = index.links.html;
-        a.target = `_blank`
+        for (let index of Result) {
+            const img = document.createElement(`img`);
+            img.src = index.urls.small;
 
-        a.append(img);
-        document.getElementById(`Pages`).append(a);
+            const a = document.createElement(`a`);
+            a.href = index.links.html;
+            a.target = `_blank`
+
+            a.append(img);
+            document.getElementById(`Pages`).append(a);
+        }
+
+    } catch (error) {
+        alert(error.message)
     }
 }
 
+// Event Listener for Search Button
 Search.addEventListener("click", function () {
-
-    if (Input.value == "") {
-        alert(`Please Enter Some Text!`)
+    if (Input.value !== "") {
+        Pages.innerHTML = ``;
+        Page = 1;
+        Retrieve();
+        Increase.style.display = "block"
+    } else {
+        alert("Please enter some text!")
     }
-
-    else {
-        try {
-            Pages.innerHTML = ``;
-            Page = 1;
-            Retrieve();
-            Increase.style.display = "block"
-            
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
 })
 
+// Event Listener for Show Button
 Increase.addEventListener("click", () => {
-    
-    try {
-        Page++
+    Page++
+    if (Page < totalPages) {
         Retrieve();
-        
-    } catch (error) {
-        console.log(error);
-        
+    } else {
+        alert("You have reached the end!")
     }
 })
